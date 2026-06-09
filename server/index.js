c4163aaf6aebd11a334a776e7ae9034b5240ed30
=paul <112735839+PaulK-maker@users.noreply.github.com>
2026-06-09 14:55:22 -0700

    fix port and cors for production

--- server/index.js
+++ server/index.js
@@ -4,22 +4,20 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
+app.use('/api/profile', require('./routes/profile'));
+
 app.get('/', (req, res) => {
   res.send('Paul Karanja API is running!');
 });
 
-app.use('/api/profile', require('./routes/profile'));
-
-// Connect to MongoDB then start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.log(err));
